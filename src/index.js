0d5f8de184ad736869bbc02e1e557de4ddb3a2bd
christophe <tremblay717@gmail.com>
2022-10-02 17:24:23 -0400

    loading node_modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "hello"
-  
+    element.id = 'content';
     return element;
   }
   
