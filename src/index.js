6e5512e3004501efd6647812153f87a701c31df0
christophe <tremblay717@gmail.com>
2022-10-02 17:51:54 -0400

    dev environment working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-    element.id = 'content';
-    return element;
-}
-
-document.body.appendChild(component());
+//Setting up the html
+const element = document.createElement('div');
+element.id = 'content';
+element.textContent = 'test'
+element.style.height = '50vh';
+element.style.backgroundColor = 'green';
+document.body.appendChild(element);
