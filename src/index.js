68419d0fa91508a47ad0c65923a23ea97fb1e835
christophe <tremblay717@gmail.com>
2022-10-05 07:07:44 -0400

    added background image for homepage

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import './style.css';
-import Icon from './icon.png';
-import Icon2 from './icon2.png';
-
+import Bbq from './bbq.jpg'
 //Setting up the html
 const element = document.createElement('div');
 element.id = 'content';
 
-const horn = new Image();
-horn.className = 'hornImage'
-horn.src = Icon;
+const bbq = new Image();
+bbq.src = Bbq;
 
-const hornTwo = new Image();
-hornTwo.className = 'hornImage';
-hornTwo.src = Icon2;
 
-const test = document.createElement('p');
-// test.textContent = 'пошто је непоштовање и презирање права човека водило варварским';
 
-test.textContent = 'hello'
 
-element.appendChild(horn);
-element.appendChild(hornTwo);
-element.appendChild(test);
 document.body.appendChild(element);
