ba5143ad57fa77fbfa123c0748fab1a38e96859b
christophe <tremblay717@gmail.com>
2022-10-04 20:08:22 -0400

    fixed image

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import './style.css';
+import Icon from './icon.png';
+import Icon2 from './icon2.png';
 
 //Setting up the html
 const element = document.createElement('div');
 element.id = 'content';
-element.textContent = 'test'
-element.style.height = '50vh';
-element.style.backgroundColor = 'green';
+
+const horn = new Image();
+horn.className = 'hornImage'
+horn.src = Icon;
+
+const hornTwo = new Image();
+hornTwo.className = 'hornImage';
+hornTwo.src = Icon2;
+
+const test = document.createElement('p');
+// test.textContent = 'пошто је непоштовање и презирање права човека водило варварским';
+
+test.textContent = 'hello'
+
+element.appendChild(horn);
+element.appendChild(hornTwo);
+element.appendChild(test);
 document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,18 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+
     },
-};
+}
