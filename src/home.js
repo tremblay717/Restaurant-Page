ba2abf4939157c522f39fdb35de7cfcb6b26657d
christophe <tremblay717@gmail.com>
2022-10-13 12:31:47 -0400

    linting

--- src/home.js
+++ src/home.js
@@ -1,48 +1,47 @@
-import SteakImg from './steak.jpg';
+import SteakImg from './steak.jpg'
 
-export default function homeSection() {
+export default function homeSection () {
+  // Changing Home Color to Orange;
+  const homeLi = document.getElementById('headerLiHome')
+  homeLi.style.color = '#F14C0B'
 
-    // Changing Home Color to Orange; 
-    const homeLi = document.getElementById('headerLiHome');
-    homeLi.style.color = '#F14C0B';
+  // Creating a Div
+  const homeDiv = document.createElement('div')
+  homeDiv.setAttribute('style', 'display:flex;')
+  homeDiv.id = 'homeDiv'
 
-    // Creating a Div 
-    const homeDiv = document.createElement('div');
-    homeDiv.setAttribute('style', 'display:flex;');
-    homeDiv.id = 'homeDiv';
+  // Left Part of Home Section
+  const leftDiv = document.createElement('div')
+  leftDiv.setAttribute('style', 'display:flex; flex-direction:column;width:50%;')
+  leftDiv.id = 'homeLeftDiv'
 
-    // Left Part of Home Section
-    const leftDiv = document.createElement('div');
-    leftDiv.setAttribute('style', 'display:flex; flex-direction:column;width:50%;');
-    leftDiv.id = 'homeLeftDiv';
+  const slogan = document.createElement('span')
+  slogan.className = 'slogan'
+  slogan.textContent = 'Enjoy our excellent grills'
+  leftDiv.appendChild(slogan)
 
-    const slogan = document.createElement('span');
-    slogan.className = 'slogan';
-    slogan.textContent = "Enjoy our excellent grills";
-    leftDiv.appendChild(slogan);
+  const subSloganOne = document.createElement('p')
+  subSloganOne.className = 'subSlogan'
+  subSloganOne.textContent = 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  leftDiv.appendChild(subSloganOne)
 
-    const subSloganOne = document.createElement('p');
-    subSloganOne.className = 'subSlogan';
-    subSloganOne.textContent = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    leftDiv.appendChild(subSloganOne);
+  const subSloganTwo = document.createElement('p')
+  subSloganTwo.className = 'subSlogan'
+  subSloganTwo.textContent = 'Fusce ut placerat orci nulla pellentesque dignissim enim sit. Ornare suspendisse sed nisi lacus sed viverra. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Enim eu turpis egestas pretium aenean. Est ante in nibh mauris cursus mattis molestie a'
+  leftDiv.appendChild(subSloganTwo)
 
-    const subSloganTwo = document.createElement('p');
-    subSloganTwo.className = 'subSlogan';
-    subSloganTwo.textContent = "Fusce ut placerat orci nulla pellentesque dignissim enim sit. Ornare suspendisse sed nisi lacus sed viverra. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Enim eu turpis egestas pretium aenean. Est ante in nibh mauris cursus mattis molestie a";
-    leftDiv.appendChild(subSloganTwo);
+  // Right Part of Home Section
+  const rightDiv = document.createElement('div')
+  rightDiv.setAttribute('style', 'display:flex; flex-direction:column;width:50%;padding: 2%;')
+  rightDiv.id = 'homeRightDiv'
 
-    // Right Part of Home Section
-    const rightDiv = document.createElement('div');
-    rightDiv.setAttribute('style', 'display:flex; flex-direction:column;width:50%;padding: 2%;');
-    rightDiv.id = 'homeRightDiv';
+  const steak = new Image()
+  steak.src = SteakImg
 
-    const steak = new Image();
-    steak.src = SteakImg;
+  rightDiv.appendChild(steak)
 
-    rightDiv.appendChild(steak);
+  homeDiv.appendChild(leftDiv)
+  homeDiv.appendChild(rightDiv)
 
-    homeDiv.appendChild(leftDiv);
-    homeDiv.appendChild(rightDiv);
-
-    return homeDiv;
+  return homeDiv
 }
